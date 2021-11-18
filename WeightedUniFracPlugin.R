10688242cebca6ded981ef6d221b3b04a5ed03a8
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-11-18 18:13:41 -0500

    Switched to a Gold standard example

--- WeightedUniFracPlugin.R
+++ WeightedUniFracPlugin.R
@@ -3,14 +3,26 @@ library(ggplot2)
 #library(phyloseq)
 library(ape)
 library(psadd)
+library(vegan)
+library(multtest)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
 
 input <- function(inputfile) {
   parameters <<- read.table(inputfile, as.is=T);
-  rownames(parameters) <<- parameters[,1]; 
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <- paste(pfix, "/", sep="")
+  }
+
+rownames(parameters) <<- parameters[,1]; 
    # Need to get the three files
-   otu.path <<- parameters["otufile", 2]
-   tree.path <<- parameters["tree", 2]
-   map.path <<- parameters["mapping", 2]
+   otu.path <<- paste(pfix, toString(parameters["otufile", 2]),sep="")
+   tree.path <<- paste(pfix, toString(parameters["tree", 2]), sep="")
+   map.path <<- paste(pfix, toString(parameters["mapping", 2]), sep="")
 }
 run <- function() {
    physeq <<- read_csv2phyloseq(otu.file=otu.path, taxonomy.file=tree.path, metadata.file=map.path)
@@ -20,7 +32,17 @@ run <- function() {
 output <- function(outputfile) {
   print("Generating plot...")
   y <- distance(physeq, method="wunifrac")
+  #meta = sample_data(physeq)
+  #meta = meta[,"Category"]
+  #print(str(meta))
+  #print(meta@.Data[1])
+  #vm = as.vector(unlist(meta@.Data[1]))
+  #print(c(vm))
+  #print(typeof(c(vm)))
+  #res = mt.minP(physeq, c(vm))
+  #print(res)
   print("Generating CSV...")
   write.csv(as.matrix(y), outputfile)
+  #write.csv(res, "lalala.csv")
 }
 
